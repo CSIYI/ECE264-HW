82cab8428624a97c09a6bce37105aef1d50b2b16
CSIYI <cai71@purdue.edu>
2014-09-08 19:14:25 -0400

    answer02.c

--- PA02/answer02.c
+++ PA02/answer02.c
@@ -12,14 +12,15 @@ size_t my_strlen(const char * str)
    return(length);
 }
 
-int my_coutchar(const char * str, char ch);
+
+int my_coutchar(const char * str, char ch)
 {  
    int length = 0;
    int index = 0;
   
    while (str[length] != '\0')
    {
-      if (str[length] = ch)
+      if (str[length] == ch)
       {
         index ++;
       }
@@ -28,13 +29,169 @@ int my_coutchar(const char * str, char ch);
    return(index);
 } 
 
+
 char * my_strchr(const char * str, int ch)
 {
-  int length = 0;
-  int index = 0;
-  while (str[length] != '\0')
-  {
-     if (str[length] = ch)
+  int length = 0; 
+  char * string = NULL;
+
+do
+{
+     if (str[length] == ch)
      {
-        
+        string = (char *)&str[length];               
+        return(string);
+     }   
+     length++;
+}while(str[length] != '\0'); 
+
+  return(string);
+}
+
+
+char *my_strrchr(const char * str, int ch)
+{
+  int length = my_strlen(str);
+  char * string = NULL;
   
+  while(str[length] != ch && length >= 0)
+  {
+    length--; 
+  }
+  if (length > 0) 
+  {     
+    string =(char *)&str[length];
+  }
+  
+  
+  return(string);
+}
+
+
+char * my_strstr(const char *haystack, const char * needle)
+{
+  int index;
+  int len = my_strlen(needle);
+  int length = 0;
+  int n = 0;   
+  
+  while(haystack[length] != '\0')
+  {
+     if(haystack[length] == needle[n])
+     {
+        for(n; haystack[length+n] == needle[n];n++)
+        {
+           if (n == len)
+           {
+             return((char*)needle);
+           }
+        }
+        return(NULL);           
+      }       
+      length++;
+   }
+  if (needle == '\0')
+  {
+      return((char*)haystack);
+  }
+
+
+char * my_strcpy(char * dest, const char * src)
+{
+  int index = 0;
+ 
+ 
+  while(src[length] != '\0')
+  {
+     dest[index] = src [index];
+     index++;
+     
+  }
+  return(dest);
+}   
+
+
+char * my_strcat(char * dest, const char * src)
+{
+  int index = 0;
+  int length = 0;
+  
+  while( dest[length] != '\0')
+  {
+     length++;
+  }
+  
+  dest[length]=' ';
+   
+  while (src[index] != '\0')
+  {
+     dest[1 + length + index]= src[index]; 
+     index++;
+  }
+   
+  return(dest);
+}
+
+int my_isspace(int ch)
+{ 
+  int index = 0;
+  if (ch == ' ' || ch == '\f' || ch =='\n' || ch == '\r' || ch == '\t' || ch == '\v')
+  {
+     index = 1;
+  } 
+  return(index);
+} 
+
+/*int my_atoi(const char * str)
+{
+  int ret = 0;
+  int index = 0;
+ 
+  while(str[index] != ' ')
+  {
+ */         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+ 
+   
